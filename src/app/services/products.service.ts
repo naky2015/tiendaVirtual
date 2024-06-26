49b3ffd23daf4055a71e711f42b6185c4240ac4b
Ignacio <pantallaazul.com@gmail.com>
2024-06-26 20:47:30 -0300

    Se modifica para que filtre directamente por categoria del backend

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -17,8 +17,8 @@ getProducts():Observable<ProducsArray>{
 getIndividualProduct(id: number): Observable<ProducsArray>{
   return this._http.get<ProducsArray>(`${this.urlBase}/productos?linkTo=id_producto&equalTo=${id}`)
 }
-getPrductsByName(name: string){
-   return this._http.get(`${this.urlBase}/productos?linkTo=producto&like=${name}`)
+getPrductsByCategoryId(id: string): Observable<ProducsArray>{
+   return this._http.get<ProducsArray>(`${this.urlBase}/productos?linkTo=categoria_id&equalTo=${id}`)
 }
 
 }
